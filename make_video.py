b80a8d03bc018d8995fe0ef62adc24695e15e9e9
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2023-06-12 17:38:10 -0400

    make video

--- make_video.py
+++ make_video.py
@@ -5,6 +5,16 @@ from os.path import join
 import os
 import glob
 import matplotlib.pyplot as plt
+from PIL import Image, ImageFont, ImageDraw
+
+def generate_video(files):
+    video_name = 'vid_020.avi'
+    frame = cv2.imread(files[0])
+    h, w, c = frame.shape
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('M','J','P','G'), 30, (w, h))
+    for f in files:
+        video.write(cv2.imread(f))
+    video.release()
 
 border = 10
 
@@ -13,6 +23,9 @@ preds = np.load(join(base_dir, "results-mine/preds.npy"))
 foldit_pred_ims = sorted(glob.glob("/playpen/CEP/results/foldit_public_020/test_latest/images/*_fake_B.png"))
 image_files = sorted(glob.glob(join(base_dir, "image/*.jpg")))
 out_dir = join(base_dir, 'video')
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 40)
+out_files = []
+
 for i,f in enumerate(image_files):
     im = skimage.io.imread(f)
     im = skimage.transform.resize(im, (preds.shape[1], preds.shape[2]))
@@ -28,15 +41,21 @@ for i,f in enumerate(image_files):
     h, w, c = im.shape
     out_im = np.ones((h+100, w*3+4*border, c), dtype=im.dtype)
     results = [im, im_seg, im_foldit]
+    captions = ['Original', 'Ours', 'FoldIt']
 
     for i in range(len(results)):
         out_im[border:border+h,border*(i+1)+i*w:(border+w)*(i+1)] = results[i]
-        # out_im[border:border+h,border*2+w:border*2+2*w] = im_seg
-        # out_im[border:border+h,border*3+2*w:border*3+3*w] = im_foldit
-    out_im = (out_im*255).astype(np.uint8)
-    out_im = cv2.cvtColor(out_im, cv2.COLOR_RGB2BGR)
-    cv2.imwrite(join(out_dir, f"{f.split('/')[-1][:-4]}_s.jpg"), out_im)
 
-    # out_im = np.hstack([im, im_seg, im_foldit])*255
-    # out_im = out_im.astype(np.uint8)
-    # skimage.io.imsave(join(out_dir, f"{f.split('/')[-1][:-4]}_s.jpg"), out_im)
+    out_im = (out_im*255).astype(np.uint8)
+    out_im = Image.fromarray(out_im)
+    drawn = ImageDraw.Draw(out_im)
+    for i in range(len(captions)):
+        lth = drawn.textlength(captions[i], font=font)
+        tx = border*(i+1) + w*i + w//2 - int(lth/2)
+        ty = border+h+20
+        drawn.text((tx,ty), captions[i], (0,0,0), font=font)
+    fname = join(out_dir, f"{f.split('/')[-1][:-4]}_s.jpg")
+    out_im.save(fname)
+    out_files.append(fname)
+
+generate_video(out_files)
