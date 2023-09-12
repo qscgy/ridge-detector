c739d4517e2af12a37a32cbf033176614e1efa79
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2023-09-12 16:14:31 -0400

    cleanup

--- infer_scribble.py
+++ infer_scribble.py
@@ -39,32 +39,36 @@ from rloss.pytorch.deeplabv3plus.NormalizedCutLoss import NormalizedCutLoss
 global grad_seg
 
 class FixedImageDataset(Dataset):
+    """Class representing a dataset of images that does not do any data augmentation.
+    Intended for use at inference time.
+    """
     def __init__(self, data_path, crop_size, in_channels=3, gt=None, pattern='*.jpg'):
         super().__init__()
 
-        if 'annotations' in data_path:
+        if 'annotations' in data_path:  # annotations file was passed in, which is keyed by file name
             with open(data_path, 'rb') as f:
                 self.images = list(pickle.load(f).keys())
-        elif data_path[-3:]=='pkl':   # data_path is a pickle
+        elif data_path[-3:]=='pkl':   # path to a pickle of file names was passed in
             with open(data_path, 'rb') as f:
                 self.images = pickle.load(f)
-        else:   # data_path is a path
+        else:   # data_path is a path to a directory of images
             self.images = glob.glob(os.path.join(data_path, pattern))
         
         self.images = natsorted(self.images)
         self.depths = None
         self.normal_paths = None
 
-        if in_channels == 4:
+        if in_channels == 4:    # using colors and depths
             self.depths = [get_depth_from_image(l) for l in self.images]
-
-        if in_channels==6:
+        elif in_channels == 6:      # using colors and normals
             with open('normal_paths_test.pkl', 'rb') as f:
                 self.normal_paths = pickle.load(f)
+        else:   # colors only
+            pass
 
         self.labels = None
 
-        if gt is not None:
+        if gt is not None:   # ground truth was provided
             with open(gt, 'rb') as f:
                 self.labels = pickle.load(f)
             
@@ -100,6 +104,7 @@ class FixedImageDataset(Dataset):
             normal = F.interpolate(normal, size=self.crop_size)
             ims = torch.cat((ims, normal), 0)
         
+        # Load scribbles
         if self.labels:
             fg_labels, bg_labels = self.labels[im_name]
             labels = np.ones((432, 540, 3))*255
@@ -139,10 +144,7 @@ def save_preds(preds, save_dir, images=None, save_np=True):
     
     for i in range(preds.shape[0]):
         im = cv2.cvtColor(preds[i].transpose(1,2,0)*255, cv2.COLOR_RGB2BGR)
-        # cv2.imshow(' ', im)
-        # cv2.waitKey(0)
         path = images[i].split('/')
-        # print(os.path.join(save_dir, f'{path[-3]}_{path[-1][:-4]}.png'))
         cv2.imwrite(os.path.join(save_dir, f'{path[-1][:-4]}.png'), im)
 
 def postprocess(preds):
@@ -152,9 +154,6 @@ def postprocess(preds):
         nblobs, labels, stats, _ = cv2.connectedComponentsWithStats(pred, connectivity=4)
 
 def main(args):
-    # Define Dataloader
-    kwargs = {'num_workers': args.workers, 'pin_memory': True}
-    
     # Define network
     model = DeepBoundary(num_classes=2,
                             backbone=args.backbone,
@@ -188,6 +187,7 @@ def main(args):
     
     model.eval()
 
+    # Load data
     kwargs = {'num_workers': args.workers, 'pin_memory': True}
     test_data = FixedImageDataset(
         data_path=args.base_dir, 
@@ -197,8 +197,11 @@ def main(args):
         pattern=f'*.{args.extension}'
     )
     test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, **kwargs)
+
+    # Define input and output arrays
     segmentations = torch.zeros((len(test_loader), args.batch_size, args.crop_size, args.crop_size))
     images = torch.zeros((len(test_loader), args.batch_size, 3, args.crop_size, args.crop_size))
+
     softmax = nn.Softmax(dim=1)
     labels = torch.zeros_like(segmentations)
     times = []
@@ -215,20 +218,12 @@ def main(args):
         times.append(elapsed)
         print(probs.shape)
         segmentations[i] = probs[:,1].detach().cpu()
-        # images[i] = unnorm.detach().cpu()
         images[i] = denormalizeimage(image[:,:3], MEAN, STDEV)/255.0
 
     segmentations = segmentations.reshape(segmentations.shape[0]*segmentations.shape[1], *segmentations.shape[2:]).numpy()
     images = images.reshape(images.shape[0]*images.shape[1], *images.shape[2:]).numpy()
     thresh = 0.3    # TODO this is important! Dropping thresh to 0.3 from 0.5 improves accuracy!
     pred = (segmentations > thresh)
-
-    # times = np.array(times)
-    # print(f"Time: {times.mean():.04f}, SD={times.std():.04f}, {1/times.mean():.04f} fps")
-    # print(f"Excluding first Time: {times[1:].mean():.04f}, SD={times[1:].std():.04f}")
-    # plt.figure()
-    # plt.plot(np.arange(len(times)), times)
-    # plt.show()
     
     foldit_pred_mask = process_foldit(f'/playpen/CEP/results/foldit_public/test_latest/images{"" if not args.sequence else "-test"}', (216, 216))
 
@@ -291,7 +286,7 @@ def main(args):
         ncol = int(np.sqrt(len(im_inds)))
 
     # Visualize predictions
-    fig, ax = plt.subplots(1, 3 if args.foldit_path is not None else 2, figsize=(20, 7))
+    _, ax = plt.subplots(1, 3 if args.foldit_path is not None else 2, figsize=(20, 7))
     plt.suptitle(f'Experiment {args.checkpoint.split("/")[-2].split("_")[-1]} with {args.backbone} backbone')
     grid_o = make_grid(torch.from_numpy(images[im_inds]), ncol).numpy().transpose(1,2,0)
     ax[0].imshow(grid_o)
@@ -316,16 +311,13 @@ def main(args):
         ax[2].axis('off')
 
     plt.savefig(os.path.join(*(args.checkpoint.split('/')[:-1]), f'eval_thresh_{thresh}.png'), bbox_inches='tight')
-    # plt.close()
     
     # Plot examples with annotations showing positives and negatives
     if args.use_examples:
         foldit_pred_mask_2 = process_foldit(f'/playpen/CEP/results/foldit_internal/test_latest/images{"" if not args.sequence else "-test"}', (216, 216))
-        foldit_pred_list_2 = foldit_pred_mask[labels.squeeze()<2].astype(np.uint8)
         foldit_preds2 = np.copy(images).transpose(1,0,2,3)
         foldit_preds2[0][foldit_pred_mask_2] = 0
         foldit_preds2 = foldit_preds2.transpose(1, 0, 2, 3)
-        grid_f2 = make_grid(torch.from_numpy(foldit_preds2[im_inds]), ncol).numpy().transpose(1,2,0)
 
         scribbles = labels[im_inds].squeeze()
         print(np.unique(scribbles))
@@ -336,10 +328,6 @@ def main(args):
             im_scribbles[i,scribbles[i]==1] = np.array([0,1,0])
         im_scribbles = im_scribbles.transpose(0,3,1,2)
         grid_s = make_grid(torch.from_numpy(im_scribbles), ncol).numpy().transpose(1,2,0)
-
-        # foldit_report2 = classification_report(label_list, foldit_pred_list_2, target_names=['Not ridge', 'Ridge'], output_dict=True)
-        # print('Foldit-UNC\n')
-        # print(pd.DataFrame(foldit_report2).transpose())
 
         rois = [((42, 157),(114,210), (0,1,0), 2),
         ((397,8), (426,71), (0,1,0), 2),
@@ -369,12 +357,10 @@ def main(args):
     preds_dir = os.path.join(args.outdir, 'results-mine')
     foldit_dir = os.path.join(args.outdir, 'results-foldit-internal')
     save_preds(segmentations, preds_dir, test_data.images if args.use_examples else None)
-    # if args.gt is not None:
-    #     if not os.path.isdir(foldit_dir) and args.foldit_path is not None:
-    #         save_preds(foldit_preds, foldit_dir, test_data.images)
+    if args.gt is not None:
+        if not os.path.isdir(foldit_dir) and args.foldit_path is not None:
+            save_preds(foldit_preds, foldit_dir, test_data.images)
 
-    # plt.show()
-    # plt.savefig(os.path.join(args.outdir, 'folds.png'))
     return None
 
 def find_best(args):
--- inferscribble.sh
+++ inferscribble.sh
@@ -4,8 +4,8 @@ CUDA_VISIBLE_DEVICES=0 python infer_scribble.py \
     --backbone mobilenet --workers 4 --n_class 2 \
     --crop_size 216 --extension png --batch-size 1 \
     --checkpoint run/pascal/mobilenet3-96/ex_0_cyrus/checkpoint.pth.tar \
-    --base-dir /bigpen/simulator_data/LEC_fast \
-    --in-chan 3 --outdir /bigpen/simulator_data/LEC_fast \
+    --base-dir /bigpen/simulator_data/CJM \
+    --in-chan 3 --outdir /bigpen/simulator_data/CJM \
     # --use-examples \
     # --foldit-path True \
     #  --gt pickles/annotations_test_2.pkl
